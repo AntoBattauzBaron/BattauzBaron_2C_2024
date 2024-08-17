d765203a6cd2f12f4ab04f0b6fc8d6ee85ccd1bd
Antonella Battauz Baron <antobattauzbaron.abb@gmail.com>
2024-08-17 19:03:50 -0300

    Se sube el ejercicio 5 del proyecto 1

---
+++ firmware/projects/guia1_ej5/main/guia1_ej5.c
@@ -0,0 +1,67 @@
+/*! @mainpage Template
+ *
+ * @section genDesc General Description
+ *
+ * Este programa cambia de estado de los GPIO según un número BCD ingresado
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 16/08/2024 | Document creation		                         |
+ *
+ * @author Battauz Barón Antonella (antobattauzbaron.abb@gmail.com)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+#include <gpio_mcu.h>
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+typedef struct
+{
+	gpio_t pin;			/*!< GPIO pin number */
+	io_t dir;			/*!< GPIO direction '0' IN;  '1' OUT*/
+} gpioConf_t;
+/*==================[internal functions declaration]=========================*/
+
+void estado_pines (gpioConf_t vector[], uint8_t digito) //Esta función setea los GPIO según el valor del dígito en BCD
+{
+	uint8_t estado;
+	for (int i = 0; i < 4; i++)
+	{
+		estado=digito>>i;
+		GPIOState(vector[i].pin, estado&1);
+	}
+}
+
+/*==================[external functions definition]==========================*/
+
+void app_main(void){
+	gpioConf_t vector [4]; //Vector de struct 
+	uint8_t digito=2; //Este valor puede cambiarse del 0 al 9 
+
+	vector[0].dir=1; //Se setean como salidas
+	vector[0].pin=GPIO_20;
+	vector[1].dir=1;
+	vector[1].pin=GPIO_21;
+	vector[2].dir=1;
+	vector[2].pin=GPIO_22;
+	vector[3].dir=1;
+	vector[3].pin=GPIO_23;
+
+	estado_pines(vector, digito);
+}
+/*==================[end of file]============================================*/
