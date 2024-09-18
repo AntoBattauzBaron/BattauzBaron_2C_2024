b58de6910441f3d799fb2dd163dcce8a76e7b224
Antonella Battauz Baron <antobattauzbaron.abb@gmail.com>
2024-09-18 19:30:56 -0300

    Se sube documentacion HTML y medición con osciloscopio en el pin ECHO

---
+++ firmware/projects/guia2_ej1/documentacion/html/guia2__ej1_8c.js
@@ -0,0 +1,12 @@
+var guia2__ej1_8c =
+[
+    [ "CONFIG_PERIOD_LECTURA", "guia2__ej1_8c.html#ad9c9cfecce8641ea90b15fe8527b0e46", null ],
+    [ "CONFIG_PERIOD_PRESIONADO", "guia2__ej1_8c.html#a66ca12c768f830b1249d5daef194ddc5", null ],
+    [ "CONFIG_PERIOD_TECLADO", "guia2__ej1_8c.html#a024500986d5df1376ace5f683140d1c9", null ],
+    [ "app_main", "guia2__ej1_8c.html#a630544a7f0a2cc40d8a7fefab7e2fe70", null ],
+    [ "activar", "guia2__ej1_8c.html#adb1e0964a051c97ec5c07a247e97b60c", null ],
+    [ "hold", "guia2__ej1_8c.html#af0129e079b57e897edf00f22cc25f3d4", null ],
+    [ "leerTeclado_task_handle", "guia2__ej1_8c.html#a2e35ba098de1c79b9152b7b7b43f26e4", null ],
+    [ "medirDistancia_task_handle", "guia2__ej1_8c.html#a62974a7c988f4b32cbb29e0db85be5ef", null ],
+    [ "teclas", "guia2__ej1_8c.html#a50987b3d7e97ff3669bca28b29087ee2", null ]
+];
