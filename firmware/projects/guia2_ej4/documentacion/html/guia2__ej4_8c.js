3fea3ab119fe03fddc234a6e99aaac4ab558f4a3
Antonella Battauz Baron <antobattauzbaron.abb@gmail.com>
2024-09-27 20:23:23 -0300

    Se sube documentacion

---
+++ firmware/projects/guia2_ej4/documentacion/html/guia2__ej4_8c.js
@@ -0,0 +1,13 @@
+var guia2__ej4_8c =
+[
+    [ "BUFFER_SIZE", "guia2__ej4_8c.html#a6b20d41d6252e9871430c242cb1a56e7", null ],
+    [ "CONFIG_REFRESH", "guia2__ej4_8c.html#afe3ecfc9df4969c95c07a975a15115b8", null ],
+    [ "CONFIG_REFRESH_ECG", "guia2__ej4_8c.html#a9aca26f3172db7e2f40b7c2d7f438ae7", null ],
+    [ "app_main", "guia2__ej4_8c.html#a630544a7f0a2cc40d8a7fefab7e2fe70", null ],
+    [ "FuncTimerA", "guia2__ej4_8c.html#afaccfed84cd1498a05ea04a65f1209b1", null ],
+    [ "FuncTimerB", "guia2__ej4_8c.html#a5088c3325764095ca222dc78c328f466", null ],
+    [ "cuentas_ECG", "guia2__ej4_8c.html#ad31f8cefa284aa34e77fa3a7a4909b7a", null ],
+    [ "ecg", "guia2__ej4_8c.html#a2dafb8b8179af8e9f4cc4df3f588e42e", null ],
+    [ "enviar_task_handle", "guia2__ej4_8c.html#a0c78bc0e2a8e25af9b20982dff3821c6", null ],
+    [ "muestrear_task_handle", "guia2__ej4_8c.html#ac8a84b30e346f9fca9dcd3764c98e05e", null ]
+];
